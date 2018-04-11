d7559a835f451c55793299a3de8bc3a988e14733
Danilo <danilopena.ba@gmail.com>
2018-04-10 22:01:07 -0300

    Add distortion evaluation
    Add SaS in the distortion evaluation.

--- algorithms/simulated_data/distortion_evaluation/DE_sas.m
+++ algorithms/simulated_data/distortion_evaluation/DE_sas.m
@@ -1,8 +1,8 @@
 % Universidade Federal do Rio Grande do Norte
 % Programa de Pos-Graduacao em Engenharia Eletrica e de Computacao
-% Parametric Evaluation - SNR
+% Parametric Evaluation - Impulsive Noise
 % Descricao: Avalia o desempenho do MUSIC em dados simulados variando a
-% SNR
+% G-SNR para ruido impusilvo modelado por SaS
 
 % data: sinal de entrada (sinal simulado)
 % snr: vetor de valores de SNR a serem analisados
@@ -15,7 +15,8 @@ close all
 
 delta = 6;
 angles = [20];
-snr = -40:5:40;
+alpha = 1.5;                    % SaS (ex.: 1.5->muito impulsivo, 1.7->medio, 1.9->pouco)
+gsnr = -40:10:60;
 
 elementNo = 10;                 % Quantidade de microfones
 fc = 1000;                      % Frequência da portadora
@@ -23,7 +24,7 @@ d = 0.08;                       % Distancia entre os sensores
 N = 200;                       % O numero de samples
 snapshot = 200;                % Tamanho da janela do snapshot
 u = 340;                        % Velocidade de propagacao da onda
-nIter = 1:100;                  % Numero de iteracoes de simulacao
+nIter = 1:1000;                  % Numero de iteracoes de simulacao
 
 % Parametros do sinal
 doa = angles/180*pi;            % Angulos
@@ -39,27 +40,24 @@ end
 A = A';                         % Matriz com P fontes (colunas) e M elementos (linhas)
 
 % Representacao do sinal recebido
-sig = exp(j*(wn*[1:N]));       % Sinal simulado amostrado 1:N
-s = A*sig;                      % Sinal multiplicado pelos atrasos
-signalPower = (1/N)*s(1,:)*s(1,:)';
-signalPower_dB = 10*log10(signalPower);
+sig = exp(1i*(wn*[1:N]));       % Sinal simulado amostrado 1:N
+x = A*sig;                      % Sinal multiplicado pelos atrasos
 
 % preallocate array
-RMSE = zeros(1,length(snr));
-aboluteError = zeros(1,length(snr));
-PD = zeros(1,length(snr));
+RMSE = zeros(1,length(gsnr));
+aboluteError = zeros(1,length(gsnr));
+PD = zeros(1,length(gsnr));
+y = zeros(size(x));
 
 n = 1;
-for snrValue = snr
+for gsnrValue = gsnr
     results = zeros(length(nIter),3);
     for iter = nIter
-        %x = s + awgn(s,snr);
-        noisePower_dB = signalPower_dB - snrValue;
-        noisePower = 10^(noisePower_dB/10);
-        noise = sqrt(noisePower/2) * (randn(size(s)) + 1j*randn(size(s)));
-        x = s + noise;
+        for i=1:10
+            y(i,:) = sas_complexo(x(i,:),alpha,gsnrValue);
+        end
 
-        data.x = x;
+        data.x = y;
         data.d = d;
         data.fc = fc;
         data.P = P;
@@ -77,22 +75,22 @@ for snrValue = snr
 end
 
 figure (1)
-plot(snr, RMSE)
-title('Parametric Evaluation - SNR')
-xlabel('SNR')
+plot(gsnr, RMSE)
+title('Parametric Evaluation - G-SNR')
+xlabel('G-SNR')
 ylabel('RMSE')
 grid on
 
 figure (2)
-plot(snr, aboluteError)
-title('Parametric Evaluation - SNR')
-xlabel('SNR')
+plot(gsnr, aboluteError)
+title('Parametric Evaluation - G-SNR')
+xlabel('G-SNR')
 ylabel('absolute error')
 grid on
 
 figure (3)
-plot(snr, PD)
-title('Parametric Evaluation - SNR')
-xlabel('SNR')
+plot(gsnr, PD)
+title('Parametric Evaluation - G-SNR')
+xlabel('G-SNR')
 ylabel('PD')
 grid on
