213a1e721833b953db3c64d36f611734775406af
Danilo Pena <danilo@dca.ufrn.br>
2018-04-27 08:20:33 -0300

    Initial main project

--- performance_evaluations/code/main.m
+++ performance_evaluations/code/main.m
@@ -8,11 +8,18 @@ function [results] = main(varargin)
 
 % TODO: step 1 - selection of simulated or real signal and its parameters
 % TODO: step 1.1 - selection of interference model (noise and channel models)
+data = selection_data(type, parameters_signal);
 
 % TODO: step 2 - selection of algorithms
+% TODO: step 2.1 - selection of performance metrics
+performance_metrics = evaluation(data, algorithm);
 
-% TODO: step 3 - selection of performance metrics
-
-% TODO: step 4 - selection how will be the output
+% TODO: step 3 - selection how will be the output
+Name = path_to_output;
+if exist(Name, 'file') == 2 %Initial step
+    plot(file)
+else
+    %Run simulation
+end
 
 end
