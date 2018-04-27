4e6c5fe89f318f365eed555ed7c6554df2ef3813
Danilo Pena <danilo@dca.ufrn.br>
2018-04-27 13:38:05 -0300

    call signal_generator in selection_data

--- performance_evaluations/code/aux_codes/selection_data.m
+++ performance_evaluations/code/aux_codes/selection_data.m
@@ -8,29 +8,38 @@
 % - varargin -
 % 
 
-function [signal] = selection_data(type, varargin)
+function [signal] = selection_data(type_of_data, varargin)
+
+if (nargin > 10), error('parameters number incorrect.');, end
 
 % FIXIT: defaultN attributed before of inputParser
 % defaultN
-defaultU = 340; % speed sound propagation
+defaultU = 340; % speed sound propagation (~340 m/s)
 defaultNoise.noise = 'gaussian';
-defaultNoise.snr = 0;
+defaultNoise.snr = 0; % signal-to-noise ratio (0 dB)
 defaultChannel.channel = 'none';
 
 inputs = inputParser;
-addRequired(inputs, 'type');
+addRequired(inputs, 'type_of_data');
 addRequired(inputs, 'angles');
-addRequired(inputs, 'M');
-addRequired(inputs, 'd');
-addRequired(inputs, 'f');
-addRequired(inputs, 'fs');
-addOptional(inputs, 'N', defaultN, @isnumeric);
-addOptional(inputs, 'u', defaultU, @isnumeric);
+addRequired(inputs, 'number_of_sensors');
+addRequired(inputs, 'distance_between_sensors');
+addRequired(inputs, 'source_frequency');
+addRequired(inputs, 'sampling_frequency');
+addOptional(inputs, 'number_of_samples', defaultN, @isnumeric);
+addOptional(inputs, 'speed_propagation', defaultU, @isnumeric);
 addParameter(inputs, 'noise', defaultNoise);
 addParameter(inputs, 'channel', defaultChannel);
 
-parse(inputs, type, varargin{:});
+parse(inputs, type_of_data, varargin{:});
 
-
+switch inputs.Results.type_of_data
+    case 'simulated'
+        signal = signal_generator(inputs.Results.angles, inputs.Results.number_of_samples, inputs.Results.number_of_sensors, inputs.Results.distance_between_sensors, inputs.Results.speed_propagation, inputs.Results.source_frequency, inputs.Results.sampling_frequency, inputs.Results.noise, inputs.Results.channel);
+    case 'real'
+        %signal = load();
+    otherwise
+        error('Type of data invalid.');
+end
 
 end
