b3919093d7372000b02cd392dc241c98bd62a4f3
Danilo Pena <danilo@dca.ufrn.br>
2018-04-27 21:00:38 -0300

    Just a test

--- performance_evaluations/code/main.m
+++ performance_evaluations/code/main.m
@@ -17,25 +17,27 @@ number_of_samples = 200;
 noise.model = 'gaussian';
 noise.snr = 0;
 
-% TODO: step 1 - selection of simulated or real signal and its parameters
-% TODO: step 1.1 - selection of interference model (noise and channel models)
-signal = selection_data(type_of_data, angles, number_of_sensors, distance_between_sensors, source_frequency, sampling_frequency, number_of_samples, 'noise', noise);
-
-[theta, pmusic] = MUSIC(signal, 1, 1000, 0.08);
-figure(1);
-plot(theta,pmusic);
-print('test','-depsc'); 
-
-% TODO: step 2 - selection of algorithms
-% TODO: step 2.1 - selection of performance metrics
-%performance_metrics = evaluation(data, algorithm);
-
-% TODO: step 3 - selection how will be the output
+% TODO: step 0 - selection how will be the output
+% TODO: step 0.1 - check if output exist
 %Name = path_to_output;
 %if exist(Name, 'file') == 2 %Initial step
 %    plot(file)
 %else
     %Run simulation
 %end
+
+% step 1 - selection of simulated or real signal and its parameters
+% step 1.1 - selection of interference model (noise and channel models)
+signal = selection_data(type_of_data, angles, number_of_sensors, distance_between_sensors, source_frequency, sampling_frequency, number_of_samples, 'noise', noise);
+
+%[theta, pmusic] = MUSIC(signal, 1, 1000, 0.08);
+%figure(1);
+%plot(theta,pmusic);
+%print('test','-depsc'); 
+
+% TODO: step 2 - selection of algorithms
+% TODO: step 2.1 - selection of performance metrics
+%performance_metrics = evaluation(data, algorithm);
+
 
 %end
