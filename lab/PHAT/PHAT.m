979997fef3a7cbe7f92fbd8205c87dcddab8a46e
Danilo Pena <danilo@dca.ufrn.br>
2018-09-12 10:59:13 -0300

    Finish version of Gcc-phat

--- lab/PHAT/PHAT.m
+++ lab/PHAT/PHAT.m
@@ -1,30 +1,49 @@
 % Federal University of Rio Grande do Norte
 % Title: Phase Transformation
 % Author: Danilo Pena
-% Description: Phase Transformation
+% Description: Phase Transform
 % x: synthetic or real signal
 % P: source numbers
 % fs: sampling frequency
 % d: distance between the elements
 
-function [output] = PHAT(x, fs, d)
+% example for validating
+% load gong;
+% refsig = y;
+% delay1 = 5;
+% delay2 = 25;
+% sig1 = delayseq(refsig,delay1);
+% sig2 = delayseq(refsig,delay2);
+% tau_est = gccphat([sig1,sig2],refsig)
+% x(1,:) = refsig';
+% x(2,:) = sig2';
+
+%function [theta] = PHAT(x, fs, d)
 
 u = 340; % sound speed
 [M,N] = size(x);
 
-sig = x(2,:);
-refsig = x(1,:);
-
 X1 = fft(x(1,:));
 X2 = fft(x(2,:));
-X2c = conj(X2);
-R = ifft((X1 .* X2c)./abs(X1 .* X2c));
-[argvalue, argmax] = max(R);
-tau = argmax; % it's necessary subtract maxshift
+NUM = (X1 .* conj(X2));
+W = max(abs(NUM),0.01); % max(abs(X1.*X2c), epsilon)
+R = ifft(NUM./W);
+%R = ifft(exp(1i*angle(X1 .* conj(X2))));
+%Rn = fftshift(R,1);
+[argvalue, argmax] = max(abs(R));
+%%[argvalue, argmax] = max(abs(fftshift(R)));
+%[argvalue, argmax] = max(fftshift(R));
+half = length(x(2,:))/2;
+%if argmax>100
+%    tau = argmax - 2*half - 1;
+%elseif argmax<100
+%    tau = argmax - half - 1; % it's necessary subtract maxshift
+%end
+tau = argmax - 2*half - 1;
 
-%tau = gccphat(x(1,:), x(2,:), 200000);
+%tau = gccphat(x(2,:)', x(1,:)', 200000);
 tdoa = tau / fs;
 
 theta = asin(tdoa / (d/u)) * (180/pi);
 
-end
+%end
--- performance_evaluations/code/didactical_codes/TDOA_simulation.m
+++ performance_evaluations/code/didactical_codes/TDOA_simulation.m
@@ -17,32 +17,36 @@ d = 0.08;                       % Distancia entre os elementos
 
 % Parametros
 doa = [20]/180*pi;           % Angulos
-N = 400;                     % O numero de samples
-fs = 20000;                    % Frequencia de amostragem (200kHz)
-wn = [pi/100]';                 % Frequencia normalizada dos sinais (1kHz)
+N = 200;                     % O numero de samples
+fs = 200000;                    % Frequencia de amostragem
+wn = [pi/100]';                 % Frequencia normalizada dos sinais
 fc = (fs*wn)/(2*pi);            % Frequencia dos sinais  em Hz
 P = length(doa);                 % Numero de fontes de sinais
-snr = 20;                        % Relacao sinal ruido
+snr = 40;                        % Relacao sinal ruido
 
 % Matriz de direcao
 A = zeros(P,M);                 % Matriz com P linhas e M colunas
 
-for k = 1:P
-    A(k,:) = exp(-1i*2*pi*fc*d*sin(doa(k))/u*[0:M-1]);
+tau = [1:M]*(2*pi*fc*d*sin(doa)/u);
+delays = tau*fs;
+
+for k = 1:M
+%    A(k,:) = exp(-1i*2*pi*fc*d*sin(doa(k))/u*[0:M-1]);
+    s(k,:) = cos(wn*[1:N]);
 end
-A = A';                         % Matriz com P fontes (colunas) e M elementos (linhas)
+%s = s';                         % Matriz com P fontes (colunas) e M elementos (linhas)
 
 % Representacao do sinal recebido
-sig = exp(1i*(wn*[1:N]));       % Sinal simulado amostrado 1:N
-s = A*sig;                      % Sinal multiplicado pelos atrasos
+%sig = exp(1i*(wn*[1:N]));       % Sinal simulado amostrado 1:N
+%s = A*sig;                      % Sinal multiplicado pelos atrasos
 signalPower = (1/N)*s(1,:)*s(1,:)';
 signalPower_dB = 10*log10(signalPower);
 noisePower_dB = signalPower_dB - snr;   % Ruido
 noisePower = 10^(noisePower_dB/10);
-noise = sqrt(noisePower/2) * (randn(size(s)) + 1j*randn(size(s)));
-x = s + noise;    % Adicionado ruido
-
-
+%noise = sqrt(noisePower/2) * (randn(size(s)) + 1j*randn(size(s)));
+noise = sqrt(noisePower) * randn(size(s));
+%x = s + noise;    % Adicionado ruido
+x = s;
 
 % Plots
 %plot(theta,Pmusic,'-k')
