24c8fbcdb116a33bfb867e61d03000874ede55d3
Danilo Pena <danilo@dca.ufrn.br>
2018-09-27 10:09:12 -0300

    TDOA with SNR

--- performance_evaluations/code/didactical_codes/TDOA_simulation.m
+++ performance_evaluations/code/didactical_codes/TDOA_simulation.m
@@ -2,46 +2,46 @@ clc
 clear
 close all
 
-M = 2;                         % Numero de elementos no arranjo
-u = 340;                        % Velocidade de propagacao
-d = 0.08;                       % Distancia entre os elementos
+%M = 2;                         % Numero de elementos no arranjo
+%u = 340;                        % Velocidade de propagacao
+%d = 0.04;                       % Distancia entre os elementos
+%doa = [20]/180*pi;           % Angulos
+%fs = 44000;                    % Frequencia de amostragem
+%tau = (d*sin(doa)/u);
+%delay = round(tau*fs);
 
-doa = [20]/180*pi;           % Angulos
-%fs = 200000;                    % Frequencia de amostragem
-%wn = [pi/100]';                 % Frequencia normalizada dos sinais
-%fc = (fs*wn)/(2*pi);            % Frequencia dos sinais  em Hz
-%P = length(doa);                 % Numero de fontes de sinais
+snr_min = -40;
+snr_step = 1;
+snr_max = 20;
 
-A = zeros(P,M);                 % Matriz com P linhas e M colunas
-
-tau = (d*sin(doa)/u);
-delay = round(tau*fs);
+n_iter = 10;
 
 load gong;
 refsig = y;
 N = length(y);
-delay1 = 5;
-delay2 = 25;
-sig1 = delayseq(refsig,delay1);
-sig2 = delayseq(refsig,delay2);
-tau_est = gccphat([sig1,sig2],refsig)
+delay = 25;
+sig = delayseq(refsig,delay);
+tau_est = gccphat(sig,refsig);
 
-s = [refsig'; sig2'];
+s = [refsig'; sig'];
 
-snr = -10;
-signalPower = (1/N)*s(1,:)*s(1,:)';
-signalPower_dB = 10*log10(signalPower);
-noisePower_dB = signalPower_dB - snr;   % Ruido
-noisePower = 10^(noisePower_dB/10);
-%noise = sqrt(noisePower/2) * (randn(size(s)) + 1j*randn(size(s)));
-noise = sqrt(noisePower) * randn(size(s));
-x = s + noise;    % Adicionado ruido
-gccphat(x(2,:)',x(1,:)')
+%tau = zeros(1, length(snr_min:snr_step:snr_max));
+k = 0;
+for snr = snr_min:snr_step:snr_max
 
-for k = 0:M-1
-    x(k+1,:) = s(1,(1+k*delay):(N+k*delay)) + noise(1,(1+k*delay):(N+k*delay));
-%    x(k+1,:) = s(1,(1+k*delay):(N+k*delay)) + noise(1,1:N);
+    k = k + 1;
+    tau_tmp = zeros(1,n_iter);
+    for iter = 1:n_iter
+        signalPower = (1/N)*s(1,:)*s(1,:)';
+        signalPower_dB = 10*log10(signalPower);
+        noisePower_dB = signalPower_dB - snr;   % Ruido
+        noisePower = 10^(noisePower_dB/10);
+        %noise = sqrt(noisePower/2) * (randn(size(s)) + 1j*randn(size(s)));
+        noise = sqrt(noisePower) * randn(size(s));
+        x = s + noise;    % Adicionado ruido
+    
+        tau_tmp(iter) = gccphat(x(2,:)',x(1,:)');
+    end
+    tau(k) = mean(tau_tmp);
+
 end
-
-
-
