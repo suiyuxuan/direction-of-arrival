bc2d35397a216b23269f7bdaf51b90d568fec53c
Danilo Pena <danilo@dca.ufrn.br>
2018-11-15 17:24:04 -0300

    Frequency domain solution for FLOS

--- lab/FLOS-PHAT/FLOS-PHAT.m
+++ lab/FLOS-PHAT/FLOS-PHAT.m
@@ -14,13 +14,14 @@ u = 340; % speed of sound
 
 % R = robustcov(x(1,:),x(2,:));
 
-%X1 = fft(x(1,:));
-%X2 = fft(x(2,:));
-%NUM = (X1 .* conj(X2));
+% Frequency domain
+X1 = fft(x(1,:));
+X2 = fft(x(2,:));
+NUM = (X1 .* conj(X2)) .* (abs(exp(-1i*)).^p) .* conj(exp(-1i*));
 %W = max(abs(NUM),0.01); % max(abs(X1.*X2c), epsilon)
 %R = ifft(NUM./W);
 
-R = (1/N)*sum( x(1,:).^(a) .* x(2,:).^(b) );
+%R = (1/N)*sum( x(1,:).^(a) .* x(2,:).^(b) ); % Time domain
 
 [argvalue, argmax] = max(abs(R));
 
