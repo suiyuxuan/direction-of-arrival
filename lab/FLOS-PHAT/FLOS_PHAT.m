3c44be3656b0a6552f30875c8a0e605d3fe48a7f
Danilo Pena <danilo@dca.ufrn.br>
2018-11-24 00:25:42 -0300

    Implemented FLOC TDOA

--- lab/FLOS-PHAT/FLOS_PHAT.m
+++ lab/FLOS-PHAT/FLOS_PHAT.m
@@ -7,25 +7,32 @@
 % f: source frequency
 % d: distance between the elements
 
-function [theta,pMusic] = FLOS-PHAT(x, P, f, d)
+function [theta] = FLOS_PHAT(x, P, f, d)
 
 [M,N] = size(x); % M - element number, N - number of samples
 u = 340; % speed of sound
-
-% R = robustcov(x(1,:),x(2,:));
+p = 0.5;
 
 % Frequency domain
-X1 = fft(x(1,:));
-X2 = fft(x(2,:));
-NUM = (X1 .* conj(X2)) .* (abs(exp(-1i*)).^p) .* conj(exp(-1i*));
-%W = max(abs(NUM),0.01); % max(abs(X1.*X2c), epsilon)
-%R = ifft(NUM./W);
+X1 = fft(x(1,:).^p);
+X2 = fft(x(2,:).^p);
+%NUM = (X1 .* conj(X2)) .* (abs(exp(-1i*)).^p) .* conj(exp(-1i*));
+NUM = (X1 .* conj(X2));
+W = max(abs(NUM),0.01); % max(abs(X1.*X2c), epsilon)
+R = ifft(NUM./W);
 
-%R = (1/N)*sum( x(1,:).^(a) .* x(2,:).^(b) ); % Time domain
+%for n=1:N
+%for m=1:N
+%R(m) = (1/N)*sum( abs(x(1,n)).^(a-1) .* x(2,n+m).^(b-1) ); % Time domain
+%end
+%end
 
 [argvalue, argmax] = max(abs(R));
 
-tau = argmax - 1;
+half = length(x(2,:))/2;
+
+%tau = argmax - 1;
+tau = -(argmax - 2*half - 1);
 tdoa = tau / fs;
 theta = asin(tdoa / (d/u)) * (180/pi);
 
