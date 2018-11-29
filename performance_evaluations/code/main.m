ad5a028069c07ed47ff3f05951c2eaf576813512
Danilo <danilopena.ba@gmail.com>
2018-11-29 13:13:07 -0300

    Added two type of simulations on main

--- performance_evaluations/code/main.m
+++ performance_evaluations/code/main.m
@@ -21,27 +21,49 @@ clc
 
 %% Param
 
-output_name = "teste";
-algorithms = ["MUSIC"];
+output_name = "teste_DOA";
+algorithms = ["MUSIC" "NLT-MUSIC"];
 type_of_data = "simulated-sine";
 angles = [20];
 number_of_sensors = 10;
 distance_between_sensors = 0.08;
 source_frequency = [1000];
 sampling_frequency = 40000;
-number_of_samples = 400; %353; %139
-number_of_iterations = 100;
+number_of_samples = 400;
+number_of_iterations = 10000;
 speed_propagation = 340;
 number_of_snapshots = 1;
-%noise{1}.model = "gaussian real";
-%noise{1}.snr = -20:40;
-noise{1}.model = "gaussian mixture complex";
+noise{1}.model = "gaussian complex";
 noise{1}.snr = -20:40;
-noise{1}.rel = 100;
-%noise{1}.model = "alpha-stable complex";
-%noise{1}.snr = -20:40; % GSNR
-%noise{1}.alpha = 1.3;
+noise{2}.model = "gaussian mixture complex";
+noise{2}.snr = -20:40;
+noise{2}.rel = 100;
+noise{3}.model = "alpha-stable complex";
+noise{3}.snr = -20:40;
+noise{3}.alpha = 1.7;
 deviation_of_angle = 6;
+
+% output_name = "teste_TDOA";
+% algorithms = ["GCC-PHAT" "GCC-NLT" "FLOS-PHAT"];
+% type_of_data = "simulated-zadoff-chu";
+% angles = [20];
+% number_of_sensors = 10;
+% distance_between_sensors = 0.08;
+% source_frequency = [1000];
+% sampling_frequency = 40000;
+% number_of_samples = 353; %139
+% number_of_iterations = 10000;
+% speed_propagation = 340;
+% number_of_snapshots = 1;
+% noise{1}.model = "gaussian real";
+% noise{1}.snr = -20:40;
+% noise{2}.model = "gaussian mixture real";
+% noise{2}.snr = -20:40;
+% noise{2}.rel = 100;
+% noise{3}.model = "alpha-stable real";
+% noise{3}.snr = -20:40;
+% noise{3}.alpha = 1.7;
+% deviation_of_angle = 6;
 
 %% Check data
 
