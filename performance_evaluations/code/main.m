d2c23ef162b9d0b9ca952ee5ab2906a7107a7967
Danilo Pena <danilo@dca.ufrn.br>
2018-11-29 10:31:04 -0300

    Get better main with comments

--- performance_evaluations/code/main.m
+++ performance_evaluations/code/main.m
@@ -3,17 +3,28 @@
 % Author: Danilo Pena
 % Description: Main function for performance evaluation
 %
+% algorithms: % "MUSIC", "ESPRIT", "Capon", "Root-MUSIC", "Beamscan", 
+% "GCC-PHAT", "GCC-NLT", "FLOS-PHAT", "NLT-MUSIC"
+%
 % type_of_data: "simulated-sine", "simulated-zadoff-chu",
 % "simulated-voice", "real", "demo-gong"
+%
+% noise{n}.model = "gaussian real", "gaussian complex",
+% "alpha-stable real", "alpha-stable complex", 
+% "gaussian mixture real", "gaussian mixture complex"
+
+%% Clear and close
 
 clear
 close all
 clc
 
-output_name = "teste_MUSIC_gmm_rel";
-algorithms = ["MUSIC"]; % "MUSIC", "ESPRIT", "Capon", "Root-MUSIC", "Beamscan", "GCC-PHAT", "GCC-NLT", "FLOS-PHAT", "NLT-MUSIC"
-type_of_data = "simulated-sine"; % "simulated-sine", "simulated-zadoff-chu", "simulated-voice", "demo-gong", "real"
-angles = [20]; % This should be a cell (combination of number of source)
+%% Param
+
+output_name = "teste";
+algorithms = ["MUSIC"];
+type_of_data = "simulated-sine";
+angles = [20];
 number_of_sensors = 10;
 distance_between_sensors = 0.08;
 source_frequency = [1000];
@@ -27,33 +38,19 @@ number_of_snapshots = 1;
 noise{1}.model = "gaussian mixture complex";
 noise{1}.snr = -20:40;
 noise{1}.rel = 100;
-noise{2}.model = "gaussian mixture complex";
-noise{2}.snr = -20:40;
-noise{2}.rel = 1000;
 %noise{1}.model = "alpha-stable complex";
 %noise{1}.snr = -20:40; % GSNR
 %noise{1}.alpha = 1.3;
-
 deviation_of_angle = 6;
 
-% step 1 - selection of simulated or real signal and its parameters
-% step 1.1 - selection of interference model (noise and channel models)
+%% Check data
+
 data = check_data(type_of_data, output_name, angles, number_of_sensors, distance_between_sensors, source_frequency, sampling_frequency, number_of_samples, speed_propagation, 'noise', noise);
 
-%[theta, pmusic] = MUSIC(signal, 1, 1000, 0.08);
-%figure(1);
-%plot(theta,pmusic);
-%print('test','-depsc'); 
+%% Evaluation of results
 
-% step 2 - selection of algorithms
-% step 2.1 - selection of performance metrics
 performance_metrics = evaluation(data, algorithms, angles, deviation_of_angle, 'repeat', number_of_iterations);
 
-%figure (1)
-%plot(performance_metrics.algorithms(:).RMSE)
-%print('test','-depsc');
+%% Save outputs and plot
 
-% step 3 - save outputs
 save_outputs(output_name, performance_metrics);
-
-%end
