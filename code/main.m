39582e2686c7a3be5f369b18cb8c3319713b85a4
Danilo Pena <danilo@dca.ufrn.br>
2019-01-30 14:27:52 -0300

    Main.m para simulacao do Mario

--- code/main.m
+++ code/main.m
@@ -29,25 +29,25 @@ addpath(genpath('didactical_codes'));
 
 % Example 1 - DOA algorithms evaluation
 output_name = "test_DOA";
-algorithms = ["MUSIC" "NLT-MUSIC"];
+algorithms = ["ESPRIT" "Root-MUSIC"];
 type_of_data = "simulated-sine";
-angles = [20];
-number_of_sensors = 10;
-distance_between_sensors = 0.08;
-source_frequency = [1000];
-sampling_frequency = 40000;
-number_of_samples = 400;
-number_of_iterations = 10000;
+angles = [18];
+number_of_sensors = 8;
+distance_between_sensors = 0.085;
+source_frequency = [2000];
+sampling_frequency = 20000;
+number_of_samples = 100;
+number_of_iterations = 1000;
 speed_propagation = 340;
 number_of_snapshots = 1;
 noise{1}.model = "gaussian complex";
-noise{1}.snr = -20:40;
-noise{2}.model = "gaussian mixture complex";
-noise{2}.snr = -20:40;
-noise{2}.rel = 100;
-noise{3}.model = "alpha-stable complex";
-noise{3}.snr = -20:40;
-noise{3}.alpha = 1.7;
+noise{1}.snr = 0:20;
+% noise{2}.model = "gaussian mixture complex";
+% noise{2}.snr = -20:40;
+% noise{2}.rel = 100;
+% noise{3}.model = "alpha-stable complex";
+% noise{3}.snr = -20:40;
+% noise{3}.alpha = 1.7;
 deviation_of_angle = 6;
 
 % Example 2 - TDOA algorithms evaluation
