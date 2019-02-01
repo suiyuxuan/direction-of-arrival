f5ef312fc991a4118ae52457eec5fbb913b207da
Danilo Pena <danilo@dca.ufrn.br>
2019-01-31 22:57:42 -0300

    Fixed the parameters. Included sampling rate

--- code/distortion_models/reverberation_model.m
+++ code/distortion_models/reverberation_model.m
@@ -1,13 +1,18 @@
 % Federal University of Rio Grande do Norte
-% Programa de Pos-Graduacao em Engenharia Eletrica e de Computacao
+% Title: Reverberation model
 % Author: Danilo Pena
-% Description: Reveberator
+% Description: Reveberation model aplied to signal
+%
+% Parameters:
+% fs: sampling frequency
+% D: PreDelay
 
-function [signal] = reverberation_model(x, a, R)
+function [signal] = reverberation_model(x, fs, D)
 
 % Matlab Solution (https://www.mathworks.com/help/audio/ref/reverberator-system-object.html)
-reverb = reverberator('PreDelay', 0.5, 'WetDryMix', 1);
-signal = step(reverb, x);
+reverb = reverberator('PreDelay', D, 'SampleRate', fs);
+signal = step(reverb, x); 
+%signal = reverb(x); % older versions
 
 % My simple solution
 %H = (alpha + z^(-R))/(1 + alpha*z^(-R)); % alpha < 1, alpha=0.8 R=4
