ba23c0c1a89e92f0d71a022d3f1d3e48a360369c
Danilo Pena <danilo@dca.ufrn.br>
2019-02-04 14:37:34 -0300

    TDOA methods

--- code/main.m
+++ code/main.m
@@ -28,50 +28,50 @@ addpath(genpath('didactical_codes'));
 %% Parameters
 
 % Example 1 - DOA algorithms evaluation
-output_name = "test_DOA";
-algorithms = ["ESPRIT" "Root-MUSIC"];
-type_of_data = "simulated-sine";
-angles = [18];
-number_of_sensors = 8;
-distance_between_sensors = 0.085;
-source_frequency = [2000];
-sampling_frequency = 20000;
-number_of_samples = 100;
-number_of_iterations = 1000;
-speed_propagation = 340;
-number_of_snapshots = 1;
-noise{1}.model = "gaussian complex";
-noise{1}.snr = 0:20;
+% output_name = "test_DOA";
+% algorithms = ["ESPRIT" "Root-MUSIC"];
+% type_of_data = "simulated-sine";
+% angles = [18];
+% number_of_sensors = 8;
+% distance_between_sensors = 0.085;
+% source_frequency = [2000];
+% sampling_frequency = 20000;
+% number_of_samples = 100;
+% number_of_iterations = 1000;
+% speed_propagation = 340;
+% number_of_snapshots = 1;
+% noise{1}.model = "gaussian complex";
+% noise{1}.snr = 0:20;
 % noise{2}.model = "gaussian mixture complex";
 % noise{2}.snr = -20:40;
 % noise{2}.rel = 100;
 % noise{3}.model = "alpha-stable complex";
 % noise{3}.snr = -20:40;
 % noise{3}.alpha = 1.7;
-deviation_of_angle = 6;
+% deviation_of_angle = 6;
 
 % Example 2 - TDOA algorithms evaluation
-% output_name = "test_TDOA";
-% algorithms = ["GCC-PHAT" "GCC-NLT" "FLOS-PHAT"];
-% type_of_data = "simulated-zadoff-chu";
-% angles = [20];
-% number_of_sensors = 2;
-% distance_between_sensors = 0.05;
-% source_frequency = [1000];
-% sampling_frequency = 40000;
-% number_of_samples = 353; %139
-% number_of_iterations = 10000;
-% speed_propagation = 340;
-% number_of_snapshots = 1;
-% noise{1}.model = "gaussian real";
-% noise{1}.snr = -20:40;
-% noise{2}.model = "gaussian mixture real";
-% noise{2}.snr = -20:40;
-% noise{2}.rel = 100;
-% noise{3}.model = "alpha-stable real";
-% noise{3}.snr = -20:40;
-% noise{3}.alpha = 1.7;
-% deviation_of_angle = 6;
+output_name = "test_TDOA";
+algorithms = ["GCC-PHAT" "GCC-NLT" "FLOS-PHAT"];
+type_of_data = "simulated-zadoff-chu";
+angles = [20];
+number_of_sensors = 2;
+distance_between_sensors = 0.05;
+source_frequency = [1000];
+sampling_frequency = 40000;
+number_of_samples = 353; %139
+number_of_iterations = 10000;
+speed_propagation = 340;
+number_of_snapshots = 1;
+noise{1}.model = "gaussian real";
+noise{1}.snr = -20:40;
+noise{2}.model = "gaussian mixture real";
+noise{2}.snr = -20:40;
+noise{2}.rel = 100;
+noise{3}.model = "alpha-stable real";
+noise{3}.snr = -20:40;
+noise{3}.alpha = 1.7;
+deviation_of_angle = 6;
 
 %% Check data
 
