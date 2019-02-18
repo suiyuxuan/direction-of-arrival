2376902efbbba410ff3d3332c6bd1493352d685e
Danilo Pena <danilo@dca.ufrn.br>
2019-02-18 14:09:34 -0300

    Implemented multiple source MUSIC

---
+++ code/didactical_codes/multiple_source/MUSIC_multiple_sources.m
@@ -0,0 +1,72 @@
+clc
+clear all
+close all
+
+%%
+% Definicao de parametros iniciais
+
+M = 10;                         % Numero de elementos no arranjo
+doa = [20 60]/180*pi;           % Angulos
+snrValue = 20;                        % Relacao sinal ruido
+
+%%
+% Caso utilize sinal modelado/criado no Matlab
+% Modelagem do sinal recebido
+
+% Parametros
+N = 200;                     % O numero de samples
+%wn = [pi/3 pi/4]';                 % Frequencia normalizada dos sinais (1kHz)
+P = length(doa);                 % Numero de fontes de sinais
+wn = pi*(1./(1+(1:P)));
+
+% Matriz de direcao
+A = zeros(P,M);                 % Matriz com P linhas e M colunas
+
+for k = 1:P
+    A(k,:) = exp(-j*2*pi*0.5*sin(doa(k))/1*[0:M-1]);
+end
+A = A';                         % Matriz com P fontes (colunas) e M elementos (linhas)
+
+% Representacao do sinal recebido
+sig = exp(j*(wn'*[1:N]));       % Sinal simulado amostrado 1:N
+s = A*sig;                      % Sinal multiplicado pelos atrasos
+signalPower = (1/N)*s(1,:)*s(1,:)';
+signalPower_dB = 10*log10(signalPower);
+noisePower_dB = signalPower_dB - snrValue;   % Ruido
+noisePower = 10^(noisePower_dB/10);
+noise = sqrt(noisePower/2) * (randn(size(s)) + 1j*randn(size(s)));
+x = s + noise;                          % Adicionando ruido
+%x = s + awgn(s,snrValue);
+
+%%
+% Encontrando o angulo
+
+% Calculo dos autovalores e autovetores
+Rx = x*x';                      % Matriz covariancia dos dados
+[AV,V] = eig(Rx);               % Autovetores e Autovalores de R respectivamente
+NN = AV(:,1:M-P);               % Selecionando subespa�o do ruido (M - P)
+
+% MUSIC
+theta = -90:0.1:90;
+    
+for ii = 1:length(theta)
+    SS = zeros(1,length(M));	% Subespa�o do sinal com P dimensoes
+        for jj = 0:M-1
+            SS(1+jj) = exp(-(j*2*jj*pi*0.5*sin(theta(ii)/180*pi)));
+        end
+    PP = SS*NN*NN'*SS';
+    Pmusic(ii) = abs(1/PP);
+end
+
+Pmusic = 10*log10(Pmusic/max(Pmusic)); % Determinando pico
+
+% Plots
+plot(theta,Pmusic,'-k')
+xlabel('Angulo \theta')
+ylabel('Funcao Espectro P(\theta) /dB')
+title('MUSIC')
+grid on
+
+[Max,angles] = findpeaks(Pmusic,theta,'SortStr','descend','NPeaks',P);
+
+angles
