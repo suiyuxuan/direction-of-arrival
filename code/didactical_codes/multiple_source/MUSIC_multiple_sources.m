c5280d129d2d7caba4120528d8a343c9949ee78b
Danilo Pena <danilo@dca.ufrn.br>
2019-02-18 14:08:36 -0300

    Implemented multiple source MUSIC method

--- code/didactical_codes/multiple_source/MUSIC_multiple_sources.m
+++ code/didactical_codes/multiple_source/MUSIC_multiple_sources.m
@@ -6,34 +6,29 @@ close all
 % Definicao de parametros iniciais
 
 M = 10;                         % Numero de elementos no arranjo
-%lambda = 150;                  % Comprimento de onda
-u = 340;                        % Velocidade de propagacao
-d = 0.08;                       % Distancia entre os elementos
-%d = lambda/2;
+doa = [20 60]/180*pi;           % Angulos
+snrValue = 20;                        % Relacao sinal ruido
 
 %%
 % Caso utilize sinal modelado/criado no Matlab
 % Modelagem do sinal recebido
 
 % Parametros
-doa = [20 60]/180*pi;           % Angulos
-N = 400000;                     % O numero de samples
-fs = 200000;                    % Frequencia de amostragem (200kHz)
-wn = [pi/100 pi/50]';                 % Frequencia normalizada dos sinais (1kHz)
-fs = (fs*wn)./(2*pi);            % Frequencia dos sinais  em Hz
+N = 200;                     % O numero de samples
+%wn = [pi/3 pi/4]';                 % Frequencia normalizada dos sinais (1kHz)
 P = length(doa);                 % Numero de fontes de sinais
-snrValue = 0;                        % Relacao sinal ruido
+wn = pi*(1./(1+(1:P)));
 
 % Matriz de direcao
 A = zeros(P,M);                 % Matriz com P linhas e M colunas
 
 for k = 1:P
-    A(k,:) = exp(-j*2*pi*fs(k)*d*sin(doa(k))/u*[0:M-1]);
+    A(k,:) = exp(-j*2*pi*0.5*sin(doa(k))/1*[0:M-1]);
 end
 A = A';                         % Matriz com P fontes (colunas) e M elementos (linhas)
 
 % Representacao do sinal recebido
-sig = exp(j*(wn*[1:N]));       % Sinal simulado amostrado 1:N
+sig = exp(j*(wn'*[1:N]));       % Sinal simulado amostrado 1:N
 s = A*sig;                      % Sinal multiplicado pelos atrasos
 signalPower = (1/N)*s(1,:)*s(1,:)';
 signalPower_dB = 10*log10(signalPower);
@@ -41,21 +36,7 @@ noisePower_dB = signalPower_dB - snrValue;   % Ruido
 noisePower = 10^(noisePower_dB/10);
 noise = sqrt(noisePower/2) * (randn(size(s)) + 1j*randn(size(s)));
 x = s + noise;                          % Adicionando ruido
-
-% Analise de Fourier do sinal modelado
-% f = [fft(x(1,:)); fft(x(2,:)); fft(x(3,:)); fft(x(4,:)); fft(x(5,:)); ...
-%     fft(x(6,:)); fft(x(7,:)); fft(x(8,:)); fft(x(9,:)); fft(x(10,:))];
-% 
-% ph(1) = angle(f(1,nfreq));
-% ph(2) = angle(f(2,nfreq));
-% ph(3) = angle(f(3,nfreq));
-% ph(4) = angle(f(4,nfreq));
-% ph(5) = angle(f(5,nfreq));
-% ph(6) = angle(f(6,nfreq));
-% ph(7) = angle(f(7,nfreq));
-% ph(8) = angle(f(8,nfreq));
-% ph(9) = angle(f(9,nfreq));
-% ph(10) = angle(f(10,nfreq));
+%x = s + awgn(s,snrValue);
 
 %%
 % Encontrando o angulo
@@ -66,14 +47,12 @@ Rx = x*x';                      % Matriz covariancia dos dados
 NN = AV(:,1:M-P);               % Selecionando subespa�o do ruido (M - P)
 
 % MUSIC
-theta = -90:0.5:90;
-
-for kk = 1:P
+theta = -90:0.1:90;
     
 for ii = 1:length(theta)
     SS = zeros(1,length(M));	% Subespa�o do sinal com P dimensoes
         for jj = 0:M-1
-            SS(1+jj) = exp(-(j*2*jj*pi*fs(kk)*d*sin(theta(ii)/180*pi))/u);
+            SS(1+jj) = exp(-(j*2*jj*pi*0.5*sin(theta(ii)/180*pi)));
         end
     PP = SS*NN*NN'*SS';
     Pmusic(ii) = abs(1/PP);
@@ -88,6 +67,6 @@ ylabel('Funcao Espectro P(\theta) /dB')
 title('MUSIC')
 grid on
 
-hold on
+[Max,angles] = findpeaks(Pmusic,theta,'SortStr','descend','NPeaks',P);
 
-end
+angles
