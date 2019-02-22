b2eb4349e4cab31b4b3e8060a889d1ec22de5274
Danilo Pena <danilo@dca.ufrn.br>
2019-02-22 11:53:58 -0300

    New comments

--- code/campaign.m
+++ code/campaign.m
@@ -3,13 +3,15 @@
 % Author: Danilo Pena
 % Description: Campaign
 %
+% Function:
 % main(input_param, output_name);
 %
-% input_param list: "param_DOA", "param_TDOA", ...
-% output_name: name of the saved folder
+% Parameters:
+% input_param list: "param_DOA", "param_TDOA", "param_TDOA_GMM_analysis", "param_TDOA_alpha_stable_analysis", "param_TDOA_samples_number_analysis"
+% output_name: output name
 
 clear
 clc
 close all
 
-main("param_TDOA", "TDOA_test");
+main("param_TDOA", "TDOA");
