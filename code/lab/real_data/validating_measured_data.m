0d2ec088442746e7f86501fbd2539b0b0a851b10
Danilo <danilopena.ba@gmail.com>
2019-03-13 21:09:59 -0300

    New scenario analyses

--- code/lab/real_data/validating_measured_data.m
+++ code/lab/real_data/validating_measured_data.m
@@ -1,16 +1,22 @@
 %% Valitading
 
-load('../../../data/respeaker/indoor/speech/data.mat');
+clear
 
-window = 75001:150000;
-window = 78001:88000;
+load('../../../data/respeaker/indoor/speech/data.mat');
+load('../../../data/respeaker/hall/speech/data.mat');
+load('../../../data/respeaker/outdoor/speech/data.mat');
+
+window = 75001:150000; % indoor
+%window = 78001:88000; % indoor
+%window = 55001:120000; % outdoor
+%window = 70001:150000; % hall
 
 x = (data.channel_1(window,2));
 plot(x)
 x(:,2) = (data.channel_2(window,2));
 x = x';
 
-X = abs(fft(x(1,:)));
+X(1,:) = abs(fft(x(1,:)));
 %plot(X(1,:))
 X(2,:) = abs(fft(x(2,:)));
 %plot(X(2,:))
@@ -20,24 +26,25 @@ X_f(1,:) = angle(fft(x(1,:)));
 X_f(2,:) = angle(fft(x(2,:)));
 %plot(X_f(2,:))
 
+[maximum,indice] = max(X(1,:));
+
 % test
 d = 0.05;
-angle = 20;
+angles = 20;
 fs = 48000;
-f = (index*fs)/length(window);
+f = (indice*fs)/length(window);
 u = 340;
 
-delta = (d * 2 * pi * f * sin(angle*(pi/180))) / u
+delta = (d * 2 * pi * f * sin(angles*(pi/180))) / u
 
-[maximum,index] = max(X(1,:));
-X_f(1,index)
-X_f(2,index)
+X_f(1,indice)
+X_f(2,indice)
 
-X_f(2,index)-X_f(1,index)
+X_f(2,indice)-X_f(1,indice)
 
 % Margin error
 
-sin((90-angle)*(pi/180))*0.8
+sin((90-angles)*(pi/180))*0.8
 
 sin_x = .72/.8
 asin_x = asin(sin_x)*(180/pi)
