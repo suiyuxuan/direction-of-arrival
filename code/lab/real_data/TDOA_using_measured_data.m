477526216c29b37a040202da981e67881e8e27ec
Danilo <danilopena.ba@gmail.com>
2019-03-27 12:59:43 -0300

    Validating measured data

--- code/lab/real_data/TDOA_using_measured_data.m
+++ code/lab/real_data/TDOA_using_measured_data.m
@@ -1,28 +1,42 @@
-%% Valitading
+% Federal University of Rio Grande do Norte
+% Title: TDOA using measured data
+% Author: Danilo Pena
+% Description: TDOA methods evaluation using measured data (source and speech signals)
 
 clear
+close all
 
+%% Load data
+
+% No source
 %load('../../../data/respeaker/indoor/no_source/data.mat');
 %load('../../../data/respeaker/hall/no_source/data.mat');
 %load('../../../data/respeaker/outdoor/no_source/data.mat');
+
+% 1 kHz source
 load('../../../data/respeaker/indoor/source/data.mat');
 load('../../../data/respeaker/hall/source/data.mat');
 load('../../../data/respeaker/outdoor/source/data.mat');
+
+% Speech signal source
 load('../../../data/respeaker/indoor/speech/data.mat');
 load('../../../data/respeaker/hall/speech/data.mat');
 load('../../../data/respeaker/outdoor/speech/data.mat');
 
+% window if necessary
 %window = 75001:150000; % indoor
 %window = 78001:88000; % indoor
 %window = 70001:150000; % hall
 %window = 55001:120000; % outdoor
 %window = 108001:116000; % outdoor
 
-x = (data.channel_1(window,2));
+% Time domain
+x(:,1) = (data.channel_1(window,2));
 plot(x)
 x(:,2) = (data.channel_2(window,2));
 x = x';
 
+% Fourier analysis
 X(1,:) = abs(fft(x(1,:)));
 %plot(X(1,:))
 X(2,:) = abs(fft(x(2,:)));
@@ -33,9 +47,10 @@ X_f(1,:) = angle(fft(x(1,:)));
 X_f(2,:) = angle(fft(x(2,:)));
 %plot(X_f(2,:))
 
+% Sensing frequency
 [maximum,indice] = max(X(1,:));
 
-% test
+% TDOA parameters
 d = 0.05;
 angles = 20;
 fs = 48000;
