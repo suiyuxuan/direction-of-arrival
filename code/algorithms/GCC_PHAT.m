72d0647345385a26e82b423e86bd9897b30dfab2
Danilo <danilopena.ba@gmail.com>
2019-03-30 17:19:14 -0300

    Removed useless comment

--- code/algorithms/GCC_PHAT.m
+++ code/algorithms/GCC_PHAT.m
@@ -6,17 +6,6 @@
 % fs: sampling frequency
 % d: distance between the elements
 
-% example for validating
-% load gong;
-% refsig = y;
-% delay1 = 5;
-% delay2 = 25;
-% sig1 = delayseq(refsig,delay1);
-% sig2 = delayseq(refsig,delay2);
-% tau_est = gccphat([sig1,sig2],refsig)
-% x(1,:) = refsig';
-% x(2,:) = sig2';
-
 function [theta] = GCC_PHAT(x, fs, d)
 
 u = 340; % sound speed
@@ -28,18 +17,9 @@ NUM = (X1 .* conj(X2));
 W = max(abs(NUM),0.01); % max(abs(X1.*X2c), epsilon)
 R = ifft(NUM./W);
 %R = ifft(exp(1i*angle(X1 .* conj(X2))));
-%Rn = fftshift(R,1);
-[argvalue, argmax] = max(abs(R));
-%%[argvalue, argmax] = max(abs(fftshift(R)));
-%[argvalue, argmax] = max(fftshift(R));
+[argvalue, argmax] = max(abs(R)); % max(fftshift(R))
 half = length(x(2,:))/2;
-% if argmax>half
-%    tau = argmax - 2*half - 1;
-% elseif argmax<half
-%    tau = argmax - half - 1; % it's necessary subtract maxshift
-% end
-tau = -(argmax - 2*half - 1);
-%tau = argmax - 1;
+tau = -(argmax - 2*half - 1); % argmax - 1;
 %tau = gccphat(x(2,:)',x(1,:)');
 tdoa = tau / fs;
 
