9b4fb4c69d59c37d3dcd7b4733864f3c9111c6f1
Danilo <danilopena.ba@gmail.com>
2019-03-30 17:20:50 -0300

    Removed the GFLOC method

--- code/simulations/param_TDOA.m
+++ code/simulations/param_TDOA.m
@@ -4,7 +4,7 @@
 % Description: TDOA
 
 %% Parameters
-algorithms = ["GCC-PHAT" "GCC-NLT" "FLOC" "GFLOC"];
+algorithms = ["GCC-PHAT" "GCC-NLT" "FLOC"];
 type_of_data = "simulated-zadoff-chu";
 angles = [20];
 number_of_sensors = 2;
