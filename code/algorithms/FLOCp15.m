8822f95bf787d2107bfd9198dfdb520f40dd2d88
Danilo <danilopena.ba@gmail.com>
2019-04-11 13:18:09 -0300

    Added parametric FLOC

---
+++ code/algorithms/FLOCp15.m
@@ -0,0 +1,36 @@
+% Federal University of Rio Grande do Norte
+% Title: Fractional Lower-Order Covariation
+% Author: Danilo Pena
+% Description: FLOC
+% x: synthetic or real signal
+% fs: sampling frequency
+% d: distance between the elements
+
+function [theta] = FLOC(x, fs, d)
+
+[M,N] = size(x); % M - element number, N - number of samples
+u = 340; % speed of sound
+p = 1.5;
+
+xm = [x(2,:) x(2,:)];
+
+R = zeros(1,N);
+for m=1:N
+    NUM = 0;
+    DEN = 0;
+    for n=1:N
+        %NUM = NUM + x(1,n).*sign(xm(1,n+m));
+        %DEN = DEN + abs(xm(1,n+m));
+        NUM = NUM + x(1,n).*(abs(xm(1,n+m)).^p-1).*sign(xm(1,n+m));
+        DEN = DEN + abs(xm(1,n+m)).^p;
+    end
+    R(m) = NUM / DEN;
+end
+
+[argvalue, argmax] = max(abs(R-mean(R)));
+tau = argmax;
+
+tdoa = tau / fs;
+theta = asin(tdoa / (d/u)) * (180/pi);
+
+end
